f5238cb090a3cad928e7ead7d79d3fdd0fa2625f
MiguelRoot-Coder <migueltecnicoinfo@gmail.com>
2022-06-18 00:37:00 -0300

    first commit

---
+++ Aula_00/ola_mundo.js
@@ -0,0 +1,2 @@
+console.log("Olá, mundo")
+//No browser: Alert("Olá, mundo")
---
+++ Aula_01/for.js
@@ -0,0 +1,8 @@
+//Clásssico:
+for(i=0;i<=10;i++){
+    console.log(i)
+}
+//Método mais usado:
+for(i=0;i<=10;i+=1){
+    console.log(i)
+}
