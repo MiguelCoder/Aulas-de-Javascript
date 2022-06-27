afcb23737fa7384f629def9e33ecb6a661505d15
MiguelRoot-Coder <migueltecnicoinfo@gmail.com>
2022-06-27 09:19:58 -0300

    Aulas 3, 4 e 5

--- Aula_03/string.js
+++ Aula_03/string.js
@@ -1,4 +1,15 @@
 let nome = 'Juca'
 let sobrenome = 'Serqueira'
+let concatenado = (nome + "\n" + sobrenome)
+let nomecompleto = nome + sobrenome
+let aspas = "\""
 
 console.log(nome.concat(sobrenome))
+console.log(nome + " " + sobrenome)
+console.log(concatenado)
+console.log(`${nome} ${sobrenome}`)
+console.log(aspas)
+console.log(nomecompleto.split(""))
+console.log(concatenado.includes("Juca"))
+console.log(concatenado.startsWith("J"))
+console.log(nomecompleto.replace("J", "j"))
---
+++ Aula_04/Number.js
@@ -0,0 +1,4 @@
+let five = 5 /3
+console.log(five)
+console.log(Math.floor(five))
+console.log(Math.ceil(five))
